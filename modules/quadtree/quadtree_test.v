d1da691c7d17a27926a4d5d357af9685310ba445
Alexander di Chiara <adc@levamail.com>
2022-10-15 19:50:49 +0200

    test format

--- modules/quadtree/quadtree_test.v
+++ modules/quadtree/quadtree_test.v
@@ -4,9 +4,9 @@ import time
 
 fn test_create() {
         mut sw := time.new_stopwatch()
-		mut qt := Quadtree{}
+        mut qt := Quadtree{}
         test := qt.create(0, 0, 1340, 640, 8, 4, 0)
-		test_clone := qt.create(0, 0, 1340, 640, 8, 4, 0)
+        test_clone := qt.create(0, 0, 1340, 640, 8, 4, 0)
         assert test == test_clone
         println('create passed')
         sw.stop()
@@ -15,9 +15,9 @@ fn test_create() {
 
 fn test_get_nodes() {
         mut sw := time.new_stopwatch()
-		mut qt := Quadtree{}
-		mut test := qt.create(0, 0, 1340, 640, 8, 4, 0)
-		test.split()
+        mut qt := Quadtree{}
+        mut test := qt.create(0, 0, 1340, 640, 8, 4, 0)
+        test.split()
         t := test.get_nodes()
         assert t.len == 4
         println('get_nodes passed')
@@ -27,15 +27,15 @@ fn test_get_nodes() {
 
 fn test_insert() {
         mut sw := time.new_stopwatch()
-		mut qt := Quadtree{}
-		mut test := qt.create(0, 0, 1340, 640, 8, 4, 0)
-		mut pt := AABB{
-				x: 100
-				y: 50
-				width: 60
-				height: 100
-		}
-		assert test.particles == []
+        mut qt := Quadtree{}
+        mut test := qt.create(0, 0, 1340, 640, 8, 4, 0)
+        mut pt := AABB{
+                x: 100
+                y: 50
+                width: 60
+                height: 100
+        }
+        assert test.particles == []
         test.insert(pt)
         assert test.particles[0] == pt
         println('insert passed')
@@ -45,36 +45,36 @@ fn test_insert() {
 
 fn test_retrieve() {
         mut sw := time.new_stopwatch()
-		mut qt := Quadtree{}
-		mut test := qt.create(0, 0, 1340, 640, 8, 4, 0)
-		mut pt := AABB{
-				x: 100
-				y: 50
-				width: 60
-				height: 100
-		}
+        mut qt := Quadtree{}
+        mut test := qt.create(0, 0, 1340, 640, 8, 4, 0)
+        mut pt := AABB{
+                x: 100
+                y: 50
+                width: 60
+                height: 100
+        }
         test.insert(pt)
-		t := test.retrieve(pt)
+        t := test.retrieve(pt)
         assert t[0] == pt
         println('retrieve passed')
         sw.stop()
         println('microseconds: $sw.elapsed().microseconds()')
 }
-	
+
 fn test_clear() {
         mut sw := time.new_stopwatch()
-		mut qt := Quadtree{}
-		mut test := qt.create(0, 0, 1340, 640, 8, 4, 0)
-		mut test_clone := qt.create(0, 0, 1340, 640, 8, 4, 0)
-		mut pt := AABB{
-				x: 100
-				y: 50
-				width: 60
-				height: 100
-		}
-		test.split()
+        mut qt := Quadtree{}
+        mut test := qt.create(0, 0, 1340, 640, 8, 4, 0)
+        mut test_clone := qt.create(0, 0, 1340, 640, 8, 4, 0)
+        mut pt := AABB{
+                x: 100
+                y: 50
+                width: 60
+                height: 100
+        }
+        test.split()
         test.insert(pt)
-		assert test != test_clone
+        assert test != test_clone
         test.clear()
         assert test == test_clone
         println('clear passed')
@@ -84,9 +84,9 @@ fn test_clear() {
 
 fn test_split() {
         mut sw := time.new_stopwatch()
-		mut qt := Quadtree{}
-		mut test := qt.create(0, 0, 1340, 640, 8, 4, 0)
-		test.split()
+        mut qt := Quadtree{}
+        mut test := qt.create(0, 0, 1340, 640, 8, 4, 0)
+        test.split()
         t := test.get_nodes()
         assert t.len == 4
         println('split passed')
@@ -96,15 +96,15 @@ fn test_split() {
 
 fn test_get_index() {
         mut sw := time.new_stopwatch()
-		mut qt := Quadtree{}
-		mut test := qt.create(0, 0, 1340, 640, 8, 4, 0)
-		mut pt := AABB{
-				x: 100
-				y: 50
-				width: 60
-				height: 100
-		}
-		test.particles << pt
+        mut qt := Quadtree{}
+        mut test := qt.create(0, 0, 1340, 640, 8, 4, 0)
+        mut pt := AABB{
+                x: 100
+                y: 50
+                width: 60
+                height: 100
+        }
+        test.particles << pt
         t := test.get_index(pt)
         assert t == [1]
         println('get_index passed')
